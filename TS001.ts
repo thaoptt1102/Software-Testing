c20493b36838b305095c682a223f8db1b8203059
thaoptt1102 <63133432+thaoptt1102@users.noreply.github.com>
2020-09-30 13:11:13 +0700

    Add files via upload

---
+++ TS001.ts
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS001</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>b4a8cea5-bc59-432e-8632-b70acfa0a6e6</testSuiteGuid>
+   <testCaseLink>
+      <guid>52a90c1a-5619-4323-b1dc-ced111dd6523</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC001</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>785b6d50-4c33-4fa3-b216-8a03c058d090</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC002</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f38d34ee-858d-437b-a1d8-9693f060ee0b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC003</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c46b56e0-d20f-4892-a591-762b7247567b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC004</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7a5c0a19-230b-4a54-b8da-cbe3779c0d89</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC005</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
